973e618941540f0ea75a6e659ffa5dacb50eb8c4
syedhaseeb <121376093+syedhaseeb2005@users.noreply.github.com>
2023-02-20 10:19:26 +0500

    Add files via upload

---
+++ javascript index of & include/app.js
@@ -0,0 +1,25 @@
+// let coldrinks=["pepsi","7up","sprite","mirinda"]
+// let askwhichcoldrink=prompt("Which coldrink you want to buy")
+
+// if(coldrinks.indexOf(askwhichcoldrink)>=0){
+//     alert(`yes ${askwhichcoldrink} is available at index number ${coldrinks.indexOf(askwhichcoldrink)}`)
+// }else{
+//     alert(`no ${askwhichcoldrink} is not available`)
+// }
+
+// let cricketer=["fakhar","amir","babar","rizwan"]
+
+// let askcricketer=prompt("Ask whick cricketer is available for match")
+
+// if(cricketer.indexOf(askcricketer)>=0 && cricketer.includes(askcricketer)){
+//     alert(`yes ${askcricketer} is available for match at index number ${cricketer.indexOf(askcricketer)} ${cricketer.includes(askcricketer)}`)
+// }else{
+//     alert(`the ${askcricketer} is not available`)}
+
+
+var student={
+    name: "haseeb",
+    age: 18,
+    address: "malir"
+}
+console.log(student)
